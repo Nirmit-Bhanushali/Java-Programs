ceaec839ad062a4a91e5f851127469b703788d07
Nirmit-Bhanushali <nirmit.bhanushali@s.amity.edu>
2026-02-05 18:22:07 +0530

    Variable folder

---
+++ variable/constant.java
@@ -0,0 +1,9 @@
+package variable;
+
+public class constant {
+    public static void main(String[] args) {
+        //this will declare the variable as "final" or "constant", which means unchangeable and read-only
+        final int myNum = 15;
+        System.out.println(myNum);
+    }
+}
---
+++ variable/var_keyword.java
@@ -0,0 +1,13 @@
+package variable;
+
+public class var_keyword {
+    public static void main(String[] args) {
+        var x = 5; // x is an int
+        System.out.println(x);
+        // var myNum = 5; // int
+        // var myDouble = 9.98; // double
+        // var myChar = 'D'; // char
+        // var myBoolean = true; // boolean
+        // var myString = "Hello"; // String
+    }
+}
